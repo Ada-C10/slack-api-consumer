f4e6b2d322f655ac6c2099c40015dd92ce86298e
Devin Helmgren <devincollege@gmail.com>
2018-10-31 09:20:15 -0700

    devins API lecture

--- lib/channel.rb
+++ lib/channel.rb
@@ -1,3 +1,15 @@
 class Channel
+  attr_reader :name, :id, :purpose, :is_archived, :members
 
+  def initialize(name, id, purpose: "", is_archived: 0, members: []  )
+    raise ArgumentError if name == nil || name == "" || id == nil || id == ""
+
+    @name = name
+    @id = id
+
+    @purpose = :purpose
+    @is_archived = :is_archived
+    @is_general = :is_archived
+    @members = :members
+  end
 end
